a2bd6290b22a3917c879d7ae3acad1173ca88549
mangul2 <tkfrhdltdj@gmail.com>
2019-11-02 12:48:53 +0900

    electron complete

---
+++ index.js
@@ -0,0 +1,28 @@
+const {app, BrowserWindow, ipcMain, Menu} = require('electron');
+
+const option = {
+    width:1200,
+    height:600,
+    resizable: false, //커서로 크기조절 false 불가능 true 가능
+    webPreferences:{
+        nodeIntegration: true,   //node.js 사용여부
+        nativeWindowOpen: true,
+    }
+};
+
+let win = null;
+
+app.on("ready", ()=>{
+    Menu.setApplicationMenu(null);
+    win = new BrowserWindow(option);
+
+    win.loadFile("index.html"); 
+});
+
+ipcMain.on("openDev", ()=>{
+    win.webContents.openDevTools(); //개발자 도구가 켜짐
+})
+
+ipcMain.on("summoner", (e, data)=>{
+    console.log(data);
+});
---
+++ main.js
@@ -0,0 +1,17 @@
+const {ipcRenderer} = require('electron');
+
+window.addEventListener("keydown", (e)=>{
+    if(e.ctrlKey && e.key.toLowerCase() == "q"){
+        ipcRenderer.send("openDev");
+    }
+});
+
+window.addEventListener("load", ()=>{
+    let name = document.querySelector("#txtName");
+    let searchBtn = document.querySelector("#search");
+
+    searchBtn.addEventListener("click", ()=>{
+        let str = name.value;
+        ipcRenderer.send("summoner", {name:str});
+    });
+});
